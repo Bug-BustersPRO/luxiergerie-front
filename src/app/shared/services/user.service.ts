4342b5e2b98cc6fb68b7fcd3d1f6add563ec975c
Mohamed Ebarhmatin <mohamed.ebarhmatin@yahoo.fr>
2024-04-08 16:14:30 +0200

    all api calls done

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,5 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client } from '../models/client.model';
+import { Role } from '../models/role.model';
+import { Room } from '../models/room.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +12,89 @@ export class UserService {
 
   constructor(private httpClient: HttpClient) { }
 
-  private url: string = "http://localhost:8090/api"
+  private url: string = "http://localhost:8090/api";
+
+  private headers = new HttpHeaders({
+    'Authorization': 'Bearer ' + localStorage.getItem('jwt-token')
+  });
 
   // Employee API - call vers le backend
 
+  public getEmployees(): Observable<any> {
+    return this.httpClient.get(`${this.url}/employee`, { headers: this.headers });
+  }
+
+  public getRolesById(id: number): Observable<any> {
+    return this.httpClient.get(`${this.url}/employee/${id}/roles`, { headers: this.headers });
+  }
+
+  public getEmployeeById(id: number): Observable<any> {
+    return this.httpClient.get(`${this.url}/employee/${id}`, { headers: this.headers });
+  }
+
   // Clients API - call vers le backend
+
+  public getClients(): Observable<any> {
+    return this.httpClient.get(`${this.url}/client`, { headers: this.headers });
+  }
+
+  public createClient(client: Client): Observable<any> {
+    return this.httpClient.post(`${this.url}/client`, client, { headers: this.headers });
+  }
+
+  public addClientToRoom(clientId: number, roleName: string): Observable<any> {
+    return this.httpClient.post(`${this.url}/client/add-room/${clientId}/with-role/${roleName}`, roleName, { headers: this.headers });
+  }
 
   // Roles API - call vers le backend
 
+  public getRoles(): Observable<any> {
+    return this.httpClient.get(`${this.url}/role`, { headers: this.headers });
+  }
+
+  public getRoleById(id: number): Observable<any> {
+    return this.httpClient.get(`${this.url}/role/${id}`, { headers: this.headers });
+  }
+
+  public addNewRole(role: Role): Observable<any> {
+    return this.httpClient.post(`${this.url}/role`, role, { headers: this.headers });
+  }
+
+  public updateRole(role: Role): Observable<any> {
+    return this.httpClient.put(`${this.url}/role/${role.id}`, role, { headers: this.headers });
+  }
+
+  public deleteRole(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.url}/role/${id}`, { headers: this.headers });
+  }
+
   // Room API - call vers le backend
+
+  public getRooms(): Observable<any> {
+    return this.httpClient.get(`${this.url}/room`, { headers: this.headers });
+  }
+
+  public getAvailableRooms(): Observable<any> {
+    return this.httpClient.get(`${this.url}/room/available`, { headers: this.headers });
+  }
+
+  public createRooms(room: Room, maxRooms: number): Observable<any> {
+    return this.httpClient.post(`${this.url}/create-multiple/${maxRooms}`, room, { headers: this.headers });
+  }
+
+  public createSpecificRoom(room: Room): Observable<any> {
+    return this.httpClient.post(`${this.url}/room`, room, { headers: this.headers });
+  }
+
+  public updateRoom(room: Room): Observable<any> {
+    return this.httpClient.put(`${this.url}/room/${room.id}`, room, { headers: this.headers });
+  }
+
+  public deleteRoom(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.url}/room/${id}`, { headers: this.headers });
+  }
+
+  public deleteAllRooms(): Observable<any> {
+    return this.httpClient.delete(`${this.url}/room/delete-all`, { headers: this.headers });
+  }
 }
