c7f1f59adaefc506388befbccf55fb8a6dbcd0fa
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2024-04-17 12:04:29 +0200

    Test with karma done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-// import { SectionFacade } from './domains/section-facade';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
 
   constructor() {}
   title = 'luxiergerie';
 
-  ngOnInit() {
-  }
 }
--- src/app/shared/components/carousel/carousel.component.spec.ts
+++ src/app/shared/components/carousel/carousel.component.spec.ts
@@ -18,4 +18,45 @@ describe('CarouselComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should start carousel on init', () => {
+    spyOn(component, 'startCarousel');
+    component.ngOnInit();
+    expect(component.startCarousel).toHaveBeenCalled();
+  });
+
+  it('should clear interval on destroy', () => {
+    component.interval = setInterval(() => {}, 1000);
+    spyOn(window, 'clearInterval');
+    component.ngOnDestroy();
+    expect(window.clearInterval).toHaveBeenCalledWith(component.interval);
+  });
+
+  it('should update currentIndex in startCarousel', (done) => {
+    component.images = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
+    component.startCarousel();
+    setTimeout(() => {
+      expect(component.currentIndex).toBe(2);
+      done();
+    }, 4100);
+  });
+
+  it('should get correct transform', () => {
+    component.currentIndex = 2;
+    expect(component.getTransform()).toBe('translateX(-200%)');
+  });
+
+  it('should go to previous slide', () => {
+    component.images = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
+    component.currentIndex = 2;
+    component.previousSlide();
+    expect(component.currentIndex).toBe(1);
+  });
+
+  it('should go to next slide', () => {
+    component.images = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
+    component.currentIndex = 1;
+    component.nextSlide();
+    expect(component.currentIndex).toBe(2);
+  });
 });
