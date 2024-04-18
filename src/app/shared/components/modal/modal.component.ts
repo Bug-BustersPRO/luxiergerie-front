2e8c6fdd8ab0caede719b891e8c64bad8d3048c7
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2024-04-18 17:59:16 +0200

    quick fix

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -10,6 +10,22 @@ import { Observable } from 'rxjs';
 })
 export class ModalComponent{
 
+  //to use modal in an other component, set this variable and function
+  //public isModalOpen: boolean = false; openModal() {this.isModalOpen = true;}
+  // call the function where you need to open the modal
+  // exemple of use of the component
+  //   <app-modal
+  //   title="Réserver ma séance"
+  //   [showModal]="isModalOpen"
+  //   (closeModalEvent)="isModalOpen = false">
+  //   <section>
+  //     <div>test data</div>
+  //     <div>test data</div>
+  //     <div>test data</div>
+  //   </section>
+  // </app-modal>
+
+
   @Input() public title: string = '';
   @Output() public closeModalEvent = new EventEmitter<void>();
   @Input() public toggleModal!: Observable<void>;
