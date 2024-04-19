88177b0d67b11ff7d47869d38604d15c12d79949
Emma <114017087+emmirose@users.noreply.github.com>
2024-04-19 16:07:54 +0200

    Lux css card (#7)
    * beginning the front
    
    * exemple archi presque fonctionnel
    
    * init config, retrives data ok
    
    * feat all models done
    
    * refacto archi and services
    
    * add API calls in core service
    
    * all api calls done
    
    * create card CSS
    
    * created cards and list of cards
    
    * cleaning
    
    * [fix]minor change on css
    
    ---------
    
    Co-authored-by: KevinRoig <kevinroig@hotmail.com>
    Co-authored-by: mohamedehn <mohamed.ebarhmatin@yahoo.fr>

--- src/app/pages/section/section-page.ts
+++ src/app/pages/section/section-page.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { SectionFacade } from 'src/app/domains/section-facade';
+import { CardsListComponent } from 'src/app/shared/components/cards-list/cards-list.component';
 import { Section } from 'src/app/shared/models/section.model';
 @Component({
   selector: 'app-section-page',
   templateUrl: './section-page.html',
-  styleUrls: ['./section-page.scss'],
+  styleUrls: ['./section-page.scss', '../../shared/components/cards-list/cards-list.component.scss'],
 })
+
 export class SectionPage implements OnInit {
   public sections: Section[] = [];
 
@@ -21,5 +23,4 @@ export class SectionPage implements OnInit {
       this.sections = sections;
     });
   }
-
 }
