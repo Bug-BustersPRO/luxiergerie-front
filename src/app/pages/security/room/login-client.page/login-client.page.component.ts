40f3d8de7d46cfa33f7fe5b0c72e3a261111bf71
ehn <mohamed.ebarhmatin@yahoo.fr>
2024-06-15 16:19:06 +0200

    work is done, update login room with hotel name and colors

--- src/app/pages/security/room/login-client.page/login-client.page.component.ts
+++ src/app/pages/security/room/login-client.page/login-client.page.component.ts
@@ -5,10 +5,11 @@ import {
   Renderer2,
   ElementRef,
   ViewChild,
-  AfterViewInit,
-  ChangeDetectorRef,
+  AfterViewInit
 } from '@angular/core';
 import * as Hammer from 'hammerjs';
+import { Hotel } from 'src/app/shared/models/hotel.model';
+import { HotelService } from 'src/app/shared/services/hotel.service';
 
 @Component({
   selector: 'app-login-client.page',
@@ -16,19 +17,31 @@ import * as Hammer from 'hammerjs';
   styleUrls: ['./login-client.page.component.scss']
 })
 export class LoginClientPageComponent implements OnInit, OnDestroy, AfterViewInit {
-  startY = 0;
-  arrowClass = '';
-  hammer!: HammerManager;
-  openModal = false;
-
+  public startY = 0;
+  public arrowClass = '';
+  public hammer!: HammerManager;
+  public openModal = false;
+  public hotel!: Hotel;
+  public hotelImageUrl!: string;
   @ViewChild('arrow') arrow!: ElementRef;
 
-  constructor(private renderer: Renderer2, private cdRef: ChangeDetectorRef) {
+  constructor(private renderer: Renderer2, private hotelService: HotelService) {
     this.renderer.setStyle(document.body, 'background', 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url("30557618.jpg")');
     this.renderer.setStyle(document.body, 'background-size', 'cover');
     this.renderer.setStyle(document.body, 'background-repeat', 'no-repeat');
     this.renderer.setStyle(document.body, 'background-attachment', 'fixed');
     this.renderer.setStyle(document.body, 'background-position', 'center');
+    this.hotelService.getHotels().subscribe(() => {
+      this.hotel = this.hotelService.hotel;
+      if (this.hotel) {
+        this.hotelService.applyColors(this.hotel?.colors);
+        this.hotelService.hotelImageUrlUpdate$.subscribe((url) => {
+          this.hotelImageUrl = url;
+        });
+      } else {
+        this.hotelService.applyColors(["#FDFBF5"]);
+      }
+    });
   }
 
   ngOnInit(): void {
