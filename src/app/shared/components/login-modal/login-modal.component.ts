e070e9550ca3f5918634db6273c99c07590d69f8
Mohamed Ebarhmatin <104645292+mohamedehn@users.noreply.github.com>
2024-06-18 09:59:11 +0200

    LUX-124 Logo and Hotel name in login (#26)
    * feature almost done, need refacto methods for hotel
    
    * refacto hotel get methods with service
    
    * work is done, update login room with hotel name and colors

--- src/app/shared/components/login-modal/login-modal.component.ts
+++ src/app/shared/components/login-modal/login-modal.component.ts
@@ -1,22 +1,18 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { LoginClient } from '../../models/loginClient.model';
 import { CommonModule } from '@angular/common';
-import {animate, state, style, transition, trigger} from "@angular/animations";
-import {catchError, of} from "rxjs";
-import { CookieService } from 'ngx-cookie-service';
+import { animate, state, style, transition, trigger } from "@angular/animations";
+import { catchError, of } from "rxjs";
 import { Hotel } from '../../models/hotel.model';
 import { HotelService } from '../../services/hotel.service';
 
 @Component({
   selector: 'app-login-modal',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    CommonModule
-  ],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login-modal.component.html',
   styleUrls: ['./login-modal.component.scss'],
   animations: [
@@ -33,33 +29,39 @@ import { HotelService } from '../../services/hotel.service';
   ]
 })
 export class LoginModalComponent {
-  loginForm: FormGroup
-  loginClient?: LoginClient
-  step: number = 1
-  invalidLogin: boolean = false
+  public loginForm: FormGroup
+  public loginClient?: LoginClient
+  public step: number = 1
+  public invalidLogin: boolean = false
   public hotel: Hotel = {} as Hotel;
-  keys = [
-    ...Array.from({length: 9}, (_, i) => ({type: 'number', value: i + 1})),
-    {type: 'action', value: 'erase', symbol: `&#8592;`},
-    {type: 'number', value: 0},
-    {type: 'action', value: 'submit', symbol: `&#10004;`}
+  public keys = [
+    ...Array.from({ length: 9 }, (_, i) => ({ type: 'number', value: i + 1 })),
+    { type: 'action', value: 'erase', symbol: `&#8592;` },
+    { type: 'number', value: 0 },
+    { type: 'action', value: 'submit', symbol: `&#10004;` }
   ]
   @Input() isOpen: boolean = false
 
   constructor(
-    private formBuilder: FormBuilder, 
-    private authService: AuthService, 
-    private router: Router, 
-    private cookieService: CookieService, 
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
     private hotelService: HotelService) {
     this.loginForm = this.formBuilder.group({
       roomNumber: ['', Validators.required],
       password: ['', Validators.required]
     });
+    this.hotelService.getHotels().subscribe(() => {
+      this.hotel = this.hotelService.hotel;
+      if (this.hotel) {
+        this.hotelService.applyColors(this.hotel?.colors);
+      } else {
+        this.hotelService.applyColors(["#FDFBF5"]);
+      }
+    });
   }
 
   ngOnInit(): void {
-    this.getHotels();
   }
 
   onKey(key: number | string) {
@@ -99,32 +101,12 @@ export class LoginModalComponent {
     }
   }
 
-  getKeyDisplay(key: {type: string, value: number | string, symbol?: string}) {
+  getKeyDisplay(key: { type: string, value: number | string, symbol?: string }) {
     return key.type === 'number' ? key.value : key.symbol
   }
 
   onEditRoomNumber() {
     this.step = 1
-  }
-
-  getHotels() {
-      this.hotelService.getHotel().subscribe({
-        next: response => {
-          this.hotel = response[0];
-          if (this.hotel !== undefined && this.hotel !== null) {
-            this.applyColors(this.hotel?.colors);
-          }
-        },
-        error: (error) => {
-          console.error(error);
-        }
-      });
-  }
-
-  applyColors(colors: string[]): void {
-    document.documentElement.style.setProperty('--primary-background-color', colors[0]);
-    document.documentElement.style.setProperty('--secondary-background-color', colors[1]);
-    document.documentElement.style.setProperty('--tertiary-background-color', colors[2]);
   }
 
 }
