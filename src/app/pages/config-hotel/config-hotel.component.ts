b542926565bd052d25514e1dba059e4ed7c4820e
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2024-07-17 16:21:54 +0200

    background img working, need test update + cleanup

--- src/app/pages/config-hotel/config-hotel.component.ts
+++ src/app/pages/config-hotel/config-hotel.component.ts
@@ -13,25 +13,32 @@ import { HotelService } from 'src/app/shared/services/hotel.service';
   standalone: true,
   imports: [CommonModule, FormsModule, ButtonComponent],
   templateUrl: './config-hotel.component.html',
-  styleUrl: './config-hotel.component.scss'
+  styleUrl: './config-hotel.component.scss',
 })
 export class ConfigHotelComponent implements OnInit {
   @ViewChild('fileInput', { static: false }) fileInput!: ElementRef;
   @Input() isCreateHotel: boolean = true;
-  public steps: string[] = ['name', 'image', 'colors', 'confirmation'];
+  public steps: string[] = ['name', 'image', 'backgroundImage', 'colors', 'confirmation'];
   public currentStep: string = 'name';
-  public hotel: Hotel = { id: '', name: '', image: [], colors: [] }
+  public hotel: Hotel = {
+    id: '',
+    name: '',
+    image: [],
+    colors: [],
+    backgroundImage: [],
+  };
   public fileName!: string;
+  public backgroundFileName!: string;
   public imageUrl: string | ArrayBuffer | null = null;
+  public backgroundImageUrl: string | ArrayBuffer | null = null;
   public firstSelectedColor: string = '#000000';
   public secondSelectedColor: string = '#000000';
   public thirdSelectedColor: string = '#000000';
-  public recommandedColors: any =
-    [
-      { name: 'Option 1', value: ['#F8F8F8', '#D4AF37', '#2C3E50'] },
-      { name: 'Option 2', value: ['#F5F5DC', '#FFFFFF', '#2ECC71'] },
-      { name: 'Option 3', value: ['#34495E', '#ECF0F1', '#AED6F1'] },
-    ]
+  public recommandedColors: any = [
+    { name: 'Option 1', value: ['#F8F8F8', '#D4AF37', '#2C3E50'] },
+    { name: 'Option 2', value: ['#F5F5DC', '#FFFFFF', '#2ECC71'] },
+    { name: 'Option 3', value: ['#34495E', '#ECF0F1', '#AED6F1'] },
+  ];
   public isFileError: boolean = false;
   public filesExtension = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif'];
   public errorMessage!: string;
@@ -40,13 +47,14 @@ export class ConfigHotelComponent implements OnInit {
     private cdRef: ChangeDetectorRef,
     private hotelService: HotelService,
     private router: Router,
-    private toastr: ToastrService) {
-  }
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.hotel.name = '';
     this.hotel.image = [];
     this.hotel.colors = [];
+    this.hotel.backgroundImage = [];
     this.getCurrentHotelConfig();
   }
 
@@ -54,18 +62,19 @@ export class ConfigHotelComponent implements OnInit {
     if (!this.isCreateHotel) {
       this.getHotelImage();
       this.hotelService.getHotel().subscribe({
-        next: response => {
+        next: (response) => {
           this.hotel.id = response[0].id;
           this.hotel.name = response[0].name;
           this.hotel.colors = response[0].colors;
           this.hotel.image.push(response[0].image);
+          this.hotel.backgroundImage.push(response[0].backgroundImage);
           this.firstSelectedColor = this.hotel.colors[0];
           this.secondSelectedColor = this.hotel.colors[1];
           this.thirdSelectedColor = this.hotel.colors[2];
         },
-        error: error => {
-          console.error('Erreur lors de la récupération de l\'hôtel :', error);
-        }
+        error: (error) => {
+          console.error("Erreur lors de la récupération de l'hôtel :", error);
+        },
       });
     }
     this.cdRef.detectChanges();
@@ -80,29 +89,50 @@ export class ConfigHotelComponent implements OnInit {
           this.imageUrl = reader.result as string;
         };
       },
-      error: error => {
+      error: (error) => {
         console.error(error);
-      }
+      },
     });
   }
 
-  goToImage(): void {
-    this.currentStep = 'image';
+  getBackgroundHotelImage(): void {
+    this.hotelService.getBackgroundHotelImage().subscribe({
+      next: (response) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(response);
+        reader.onloadend = () => {
+          this.backgroundImageUrl = reader.result as string;
+        };
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
   }
 
-  goToTitle(): void {
-    this.currentStep = 'name';
-  }
-
-  goToColors(): void {
-    this.currentStep = 'colors';
-  }
-
-  goToConfirmation(): void {
-    this.currentStep = 'confirmation';
-    this.hotel.colors[0] = this.firstSelectedColor;
-    this.hotel.colors[1] = this.secondSelectedColor;
-    this.hotel.colors[2] = this.thirdSelectedColor;
+  goToStep(step: string): void {
+    switch (step) {
+      case 'image':
+        this.currentStep = 'image';
+        break;
+      case 'title':
+        this.currentStep = 'name';
+        break;
+      case 'colors':
+        this.currentStep = 'colors';
+        break;
+      case 'backgroundImage':
+        this.currentStep = 'backgroundImage';
+        break;
+      case 'confirmation':
+        this.currentStep = 'confirmation';
+        this.hotel.colors[0] = this.firstSelectedColor;
+        this.hotel.colors[1] = this.secondSelectedColor;
+        this.hotel.colors[2] = this.thirdSelectedColor;
+        break;
+      default:
+        break;
+    }
   }
 
   selectFile(): void {
@@ -119,7 +149,8 @@ export class ConfigHotelComponent implements OnInit {
         return;
       }
       if (!this.filesExtension.includes(input.files[0].type)) {
-        this.errorMessage = 'Le format du fichier doit être de type: png, jpg, jpeg ou gif';
+        this.errorMessage =
+          'Le format du fichier doit être de type: png, jpg, jpeg ou gif';
         this.isFileError = true;
         return;
       }
@@ -133,6 +164,40 @@ export class ConfigHotelComponent implements OnInit {
       const reader = new FileReader();
       reader.onload = (e) => {
         this.imageUrl = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+    this.cdRef.detectChanges();
+  }
+
+  selectBackgroundFile(): void {
+    this.fileInput.nativeElement.click();
+  }
+
+  onBackgroundFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      if (input.files[0].size > 1000000) {
+        this.isFileError = true;
+        this.errorMessage = 'La taille du fichier doit être inférieure à 1 Mo';
+        return;
+      }
+      if (!this.filesExtension.includes(input.files[0].type)) {
+        this.errorMessage =
+          'Le format du fichier doit être de type: png, jpg, jpeg ou gif';
+        this.isFileError = true;
+        return;
+      }
+
+      const file = input.files[0];
+      this.isFileError = false;
+      this.backgroundFileName = file.name;
+      this.hotel.backgroundImage = [];
+      this.hotel.backgroundImage.push(file);
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.backgroundImageUrl = reader.result;
       };
       reader.readAsDataURL(file);
     }
@@ -172,6 +237,9 @@ export class ConfigHotelComponent implements OnInit {
     if (this.hotel.image) {
       formData.append('image', this.hotel.image[0]);
     }
+    if (this.hotel.backgroundImage) {
+      formData.append('backgroundImage', this.hotel.backgroundImage[0]);
+    }
     this.hotel.colors.forEach((color) => {
       formData.append('colors', color);
     });
@@ -184,27 +252,24 @@ export class ConfigHotelComponent implements OnInit {
   }
 
   submitHotel() {
-    this.createUpdateHotel().subscribe(
-      {
-        next: response => {
-          console.log(response);
-          this.router.navigate(['/admin']);
-          this.hotelService.emitHotelUpdate(this.hotel);
-          this.toastr.success('Hôtel configuré avec succès');
-          this.cdRef.detectChanges();
-        },
-        error: error => {
-          if (this.isCreateHotel === true) {
-            console.error('Erreur lors de la création de l\'hôtel :', error);
-            this.toastr.error('Erreur lors de la création de l\'hôtel');
-          } else {
-            console.error('Erreur lors de la mise à jour de l\'hôtel :', error);
-            this.toastr.error('Erreur lors de la mise à jour de l\'hôtel');
-          }
+    this.createUpdateHotel().subscribe({
+      next: (response) => {
+        console.log(response);
+        this.router.navigate(['/admin']);
+        this.hotelService.emitHotelUpdate(this.hotel);
+        this.toastr.success('Hôtel configuré avec succès');
+        this.cdRef.detectChanges();
+      },
+      error: (error) => {
+        if (this.isCreateHotel === true) {
+          console.error("Erreur lors de la création de l'hôtel :", error);
+          this.toastr.error("Erreur lors de la création de l'hôtel");
+        } else {
+          console.error("Erreur lors de la mise à jour de l'hôtel :", error);
+          this.toastr.error("Erreur lors de la mise à jour de l'hôtel");
         }
-      }
-    );
+      },
+    });
     this.cdRef.detectChanges();
   }
-
 }
--- src/app/shared/models/hotel.model.ts
+++ src/app/shared/models/hotel.model.ts
@@ -2,10 +2,10 @@ export class Hotel {
   public id: string;
   public name: string;
   public image: File[];
-  public backgroundImage: File;
+  public backgroundImage: File[];
   public colors: string[];
 
-  constructor(id: string, name: string, image: File[], colors: string[], backgroundImage: File) {
+  constructor(id: string, name: string, image: File[], colors: string[], backgroundImage: File[]) {
     this.id = id;
     this.name = name;
     this.image = image;
