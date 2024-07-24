e12daccfe581ca8e02221ffa81e459479e562e09
Emma Rosso <emmarosso.pro@gmail.com>
2024-07-24 16:28:02 +0200

    tests CategoryPage et AccommodationPage OK

--- src/app/pages/accommodation-page/accommodation-page.component.spec.ts
+++ src/app/pages/accommodation-page/accommodation-page.component.spec.ts
@@ -3,24 +3,42 @@ import { AccommodationPageComponent} from './accommodation-page.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from 'src/app/shared/components/button/button.component';
+import { AccommodationService } from 'src/app/shared/services/accommodation.service';
+import { Accommodation } from 'src/app/shared/models/accommodation.model';
+import { of } from 'rxjs';
 
 describe('AccommodationPage', () => {
   let component: AccommodationPageComponent;
   let fixture: ComponentFixture<AccommodationPageComponent>;
+  let accommodationService: jasmine.SpyObj<AccommodationService>;
 
   beforeEach(() => {
+    const accommodationServiceSpy = jasmine.createSpyObj('AccommodationService', ['getAccommodationsByCategory']);
     TestBed.configureTestingModule({
       declarations: [AccommodationPageComponent],
       imports: [HttpClientTestingModule,
         RouterModule.forRoot([]),
-        ButtonComponent]
+        ButtonComponent],
+         providers: [
+          { provide: AccommodationService, useValue: accommodationServiceSpy }
+        ]
     });
     fixture = TestBed.createComponent(AccommodationPageComponent);
     component = fixture.componentInstance;
+    accommodationService = TestBed.inject(AccommodationService) as jasmine.SpyObj<AccommodationService>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch accommodations by category', () => {
+    const mockAccommodations: Accommodation[] = [{ reservable: true }, { reservable: false }] as Accommodation[];
+    accommodationService.getAccommodationsByCategory.and.returnValue(of(mockAccommodations));
+
+    component.getAccommodationsByCategory('1');
+    expect(accommodationService.getAccommodationsByCategory).toHaveBeenCalledWith('1');
+    expect(component.accommodations).toBe(mockAccommodations);
   });
 });
